b8d306d02e722501969f1196a0db96b783c57eab
seemapoudel58 <simapoudel6@gmail.com>
2024-07-18 19:37:18 +0545

    herosection complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import Header from './components/Header/Header';
+import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
+import "./App.css";
 
 function App() {
   return (
-  <div className="App">
-    <Header />
-
-  </div>
+    <div className="App">
+      <div className="white-gradient"></div>
+      <Header />
+      <Hero />
+    </div>
   );
 }
 
