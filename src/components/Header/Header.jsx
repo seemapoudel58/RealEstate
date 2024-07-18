75c646929c7de4e74c3ce12704e28c93377e9f64
seemapoudel58 <simapoudel6@gmail.com>
2024-07-18 10:39:06 +0545

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import './Header.css'
+const Header = () => {
+  const menuItems = [
+    { href: '#', text: 'Residencies' },
+    { href: "#", text: "Our Value" },
+    { href: "#", text: "Contact US" },
+    { href: "#", text: "Get Started" },
+    { href: "#", text: "Contact", isButton: true }
+  ];
+
+  const renderItems = (item, index) => { 
+    if (item.isButton) {
+      return (
+        <button key={index} className="button">
+          <a href={item.href}>{item.text}</a>
+        </button>
+      );
+    }
+    return (
+      <a key={index} href={item.href}>{item.text}</a>
+    );
+  };
+
+  return (
+    <section className="h-wrapper">
+      <div className="flexCenter paddings innerWidth h-container">
+        <img src="./logo.png" alt="logo" width={100} />
+        <div className=" flexCenter h-menu">
+          {menuItems.map(renderItems)}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Header;
