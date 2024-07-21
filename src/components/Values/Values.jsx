b7dac053c553cb3cff0d54d1fa26e8a1e30bd82f
seemapoudel58 <simapoudel6@gmail.com>
2024-07-21 12:45:44 +0545

    modified the code of value section

--- src/components/Values/Values.jsx
+++ src/components/Values/Values.jsx
@@ -1,66 +1,56 @@
-import React, { useState } from 'react'
-import { Accordion, AccordionItem, AccordionItemHeading, AccordionItemButton, AccordionItemPanel, AccordionItemState} from 'react-accessible-accordion';
-import "react-accessible-accordion/dist/fancy-example.css";
-import {MdOutlineArrowDropDown, MdOutlineArrowDropDownCircle} from "react-icons/md";
-import data from "../../utils/accordion.jsx";
+import React, { useState } from 'react';
+import { MdOutlineArrowDropDown, MdOutlineArrowDropUp } from "react-icons/md";
+import data from "../../utils/accordion.jsx"; // Adjust the path based on your project structure
 import "./Values.css";
 
 const Values = () => {
+  const [visibleIndex, setVisibleIndex] = useState(null);
+
+  const toggleVisibility = (index) => {
+    setVisibleIndex(visibleIndex === index ? null : index);
+  };
+
   return (
     <section className="v-wrapper">
-        <div className="paddings innerWidth flexCenter v-container">
-            <div className="v-left">
-                <div className="image-container">
-                    <img src="./value.png" alt="" />
-                </div>
-            </div>
-            <div className="flexColStart v-right">
-                <span className="orangeText">Our Value</span>
-                <span className="primaryText">Value We Give to You</span>
-                <span className="secondaryText" style={{ marginBottom: '1rem' }} >We always ready to help by providing the best services for you.
-                <br/>
-                We believe a good place to live can make your life better.
-                </span>
-
-
-                <Accordion 
-                className='accordian'
-                allowMultipleExpanded={false}
-                preExpanded={[0]}
-                >
-                    {data.map((item, index) => {
-
-                        const [className, setClassName] = useState(null)
-
-                        return (
-                            <AccordionItem key={index} uuid={index} className={`accordionItem ${className}`}>
-                                <AccordionItemHeading>
-                                    <AccordionItemButton className=' flexCenter accordionButton' >
-                                        <AccordionItemState>
-                                            {({expanded})=> expanded ? setClassName('expanded') : setClassName('collapsed')}
-                                        </AccordionItemState>
-                                        <div className="flexCenter icon">{item.icon}</div>
-                                        <span className='primaryText'>
-                                            {item.heading}
-                                        </span>
-                                        <div className="flexCenter icon">
-                                            <MdOutlineArrowDropDown size={20} />
-                                        </div>
-                            
-                                    </AccordionItemButton>
-                                </AccordionItemHeading>
-
-                                <AccordionItemPanel>
-                                    <p className="secondaryText">{item.detail}</p>
-                                </AccordionItemPanel>
-                            </AccordionItem>
-                        )
-                    })}
-                </Accordion>
-            </div>
+      <div className="paddings innerWidth v-container">
+        <div className="v-left">
+          <div className="image-container">
+            <img src="./value.png" alt="value" />
+          </div>
         </div>
+        <div className="flexColStart v-right">
+          <span className="orangeText">Our Value</span>
+          <span className="primaryText">Value We Give to You</span>
+          <span className="secondaryText" >
+            We always ready to help by providing the best services for you.
+            <br />
+            We believe a good place to live can make your life better.
+          </span>
+
+          <div className="accordion ">
+            {data.map((item, index) => (
+              <div key={index} className={`accordionItem ${visibleIndex === index ? 'expanded' : 'collapsed'}`}>
+                <div className="accordionHeader flexCenter accordionButton">
+                  <div className=" icon">{item.icon}</div>
+                  <span className='primaryText'>
+                    {item.heading}
+                  </span>
+                  <div className="icon" onClick={() => toggleVisibility(index)}>
+                    {visibleIndex === index ? <MdOutlineArrowDropUp size={20} /> : <MdOutlineArrowDropDown size={20} />}
+                  </div>
+                </div>
+                {visibleIndex === index && (
+                  <div className="accordionBody">
+                    <p className="secondaryText">{item.detail}</p>
+                  </div>
+                )}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </section>
-  )
+  );
 }
 
-export default Values
+export default Values;
