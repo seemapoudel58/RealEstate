3a111874e12aacaff753226d698cdcabbf76b1c1
seemapoudel58 <simapoudel6@gmail.com>
2024-07-23 10:31:12 +0545

    apply animation to the hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,8 +1,7 @@
-import React from "react";
 import "./Hero.css";
 import { HiLocationMarker } from "react-icons/hi";
 import CountUp from "react-countup";
-
+import { motion } from "framer-motion";
 const Hero = () => {
   return (
     <section className="hero-wrapper">
@@ -10,18 +9,22 @@ const Hero = () => {
         <div className="flexColStart hero-left">
           <div className="hero-title">
             <div className="orange-circle" />
-
-            <h1>
+            <motion.h1
+            initial={{ y: "2rem", opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{
+              duration: 2,
+              type: "ease-in",
+            }}
+            >
               Discover <br />
-              Most Suitable <br /> Property
-            </h1>
+              Most Suitable
+              <br /> Property
+            </motion.h1>
           </div>
-
-          <div className="flexColStart secondaryText  hero-des">
-            <span style={{ paddingBottom: "0.5rem" }}>
-              Find a variety of properties that suit you very easily.
-            </span>
-            <span>Forget all difficulties in finding a residence for you.</span>
+          <div className="flexColStart secondaryText flexhero-des">
+            <span>Find a variety of properties that suit you very easilty</span>
+            <span>Forget all difficulties in finding a residence for you</span>
           </div>
 
           <div className="flexCenter search-bar">
@@ -31,34 +34,41 @@ const Hero = () => {
           </div>
 
           <div className="flexCenter stats">
-            <div className="flexColStart  stat">
-              <span style={{ paddingBottom: "0.25rem" }}>
-                <CountUp start={8800} end={9000} duration={4} />
-                <span style={{ marginLeft: "0.5rem" }}>+</span>
+            <div className="flexColCenter stat">
+              <span>
+                <CountUp start={8800} end={9000} duration={4} /> <span>+</span>
               </span>
-              <span className="secondaryText">Premium Products</span>
+              <span className="secondaryText">Premium Product</span>
             </div>
-            <div className="flexColStart stat">
-              <span style={{ paddingBottom: "0.25rem" }}>
-                <CountUp start={1950} end={2000} duration={4} />
-                <span style={{ marginLeft: "0.5rem" }}>+</span>
+
+            <div className="flexColCenter stat">
+              <span>
+                <CountUp start={1950} end={2000} duration={4} /> <span>+</span>
               </span>
               <span className="secondaryText">Happy Customer</span>
             </div>
-            <div className="flexColStart stat">
-              <span style={{ paddingBottom: "0.25rem" }}>
-                <CountUp end={28} duration={4} />
-                <span style={{ marginLeft: "0.5rem" }}>+</span>
+
+            <div className="flexColCenter stat">
+              <span>
+                <CountUp end={28} /> <span>+</span>
               </span>
-              <span className="secondaryText">Award Winning</span>
+              <span className="secondaryText">Awards Winning</span>
             </div>
           </div>
         </div>
 
         <div className="flexCenter hero-right">
-          <div className="image-container">
-            <img src="./hero-image.png" alt="home" />
-          </div>
+          <motion.div
+            initial={{ x: "7rem", opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{
+              duration: 2,
+              type: "ease-in",
+            }}
+            className="image-container"
+          >
+            <img src="./hero-image.png" alt="houses" />
+          </motion.div>
         </div>
       </div>
     </section>
